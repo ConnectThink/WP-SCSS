1453be2ade88365c59926bd20ea6eddd0d3f51e0
Adam Patarino <adampatarino@gmail.com>
2014-05-06 21:41:45 -0400

    Update to address issue #9
    Adding recursive iterator to loop through sub folders in the scss directory.

--- class/class-wp-scss.php
+++ class/class-wp-scss.php
@@ -1,22 +1,22 @@
-<?php 
+<?php
 
 class Wp_Scss {
   /**
    * Compiling preferences properites
-   * 
+   *
    * @var string
    * @access public
    */
   public $scss_dir, $css_dir, $compile_method, $scssc, $compile_errors;
-   
+
 
   /**
    * Set values for Wp_Scss::properties
-   * 
+   *
    * @param string scss_dir - path to source directory for scss files
    * @param string css_dir - path to output directory for css files
    * @param string method - type of compile (compressed, expanded, etc)
-   * 
+   *
    * @var object scssc - instantiate the compiling object.
    *
    * @var array compile_errors - catches errors from compile
@@ -29,35 +29,35 @@ class Wp_Scss {
     global $scssc;
     $scssc = new scssc();
     $scssc->setFormatter($compile_method);
-    $scssc->setImportPaths($scss_dir); 
+    $scssc->setImportPaths($scss_dir);
 
     $this->compile_errors = array();
   }
 
- /** 
+ /**
    * METHOD COMPILE
    * Loops through scss directory and compilers files that end
    * with .scss and do not have '_' in front.
    *
-   * @function compiler - passes input content through scssphp, 
+   * @function compiler - passes input content through scssphp,
    *                      puts compiled css into cache file
    *
    * @var array input_files - array of .scss files with no '_' in front
    * @var array sdir_arr - an array of all the files in the scss directory
-   * 
-   * @return nothing - Puts successfully compiled css into apporpriate location 
+   *
+   * @return nothing - Puts successfully compiled css into apporpriate location
    *                   Puts error in 'compile_errors' property
    * @access public
    */
   public function compile() {
       global $scssc, $cache;
       $cache = WPSCSS_PLUGIN_DIR . '/cache/';
-      
+
       //Compiler - Takes scss $in and writes compiled css to $out file
       // catches errors and puts them the object's compiled_errors property
-      function compiler($in, $out, $instance) {  
-        global $scssc, $cache;  
-        
+      function compiler($in, $out, $instance) {
+        global $scssc, $cache;
+
         if (is_writable($cache)) {
           try {
               $css = $scssc->compile(file_get_contents($in));
@@ -78,27 +78,27 @@ class Wp_Scss {
         }
       }
 
-      $input_files = array();      
+      $input_files = array();
       // Loop through directory and get .scss file that do not start with '_'
       foreach(new DirectoryIterator($this->scss_dir) as $file) {
         if (substr($file, 0, 1) != "_" && pathinfo($file->getFilename(), PATHINFO_EXTENSION) == 'scss') {
           array_push($input_files, $file->getFilename());
         }
       }
-      
+
       // For each input file, find matching css file and compile
       foreach ($input_files as $scss_file) {
         $input = $this->scss_dir.$scss_file;
         $outputName = preg_replace("/\.[^$]*/",".css", $scss_file);
         $output = $this->css_dir.$outputName;
-        
+
         compiler($input, $output, $this);
       }
 
       if (count($this->compile_errors) < 1) {
         if  ( is_writable($this->css_dir) ) {
           foreach (new DirectoryIterator($cache) as $cache_file) {
-            if ( pathinfo($cache_file->getFilename(), PATHINFO_EXTENSION) == 'css') { 
+            if ( pathinfo($cache_file->getFilename(), PATHINFO_EXTENSION) == 'css') {
               file_put_contents($this->css_dir.$cache_file, file_get_contents($cache.$cache_file));
               unlink($cache.$cache_file->getFilename()); // Delete file on successful write
             }
@@ -111,10 +111,10 @@ class Wp_Scss {
           array_push($this->compile_errors, $errors);
         }
       }
-  } 
+  }
 
 
-  /** 
+  /**
    * METHOD NEEDS_COMPILING
    * Gets the most recently modified file in the scss directory
    * and compares that do the most recently modified css file.
@@ -126,60 +126,60 @@ class Wp_Scss {
    *
    * @var array sdir_arr - scss directory files
    * @var array cdir_arr - css directory files
-   * 
+   *
    * @var string latest_scss - file mod time of the most recent file change
    * @var string latest_css - file mod time of the most recent file change
-   * 
+   *
    * @return bool - true if compiling is needed
    */
     public function needs_compiling() {
       $latest_scss = 0;
       $latest_css = 0;
 
-      foreach ( new DirectoryIterator($this->scss_dir) as $sfile ) {
+      foreach ( new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->scss_dir)) as $sfile ) {
         if (pathinfo($sfile->getFilename(), PATHINFO_EXTENSION) == 'scss') {
           $file_time = $sfile->getMTime();
 
           if ( (int) $file_time > $latest_scss) {
-            $latest_scss = $file_time; 
+            $latest_scss = $file_time;
           }
         }
       }
 
-      foreach ( new DirectoryIterator($this->css_dir) as $cfile ) {
+      foreach ( new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->css_dir)) as $cfile ) {
         if (pathinfo($cfile->getFilename(), PATHINFO_EXTENSION) == 'css') {
           $file_time = $cfile->getMTime();
-        
+
           if ( (int) $file_time > $latest_css) {
-            $latest_css = $file_time; 
+            $latest_css = $file_time;
           }
         }
       }
-      
+
       if ($latest_scss > $latest_css) {
         return true;
       } else {
-        return false; 
+        return false;
       }
     }
 
-  /** 
+  /**
    * METHOD ENQUEUE STYLES
    * Enqueues all styles in the css directory.
    *
    * @param $css_folder - directory from theme root. We need this passed in separately
-   *                      so it can be used in a url, not path   
+   *                      so it can be used in a url, not path
    */
   public function enqueue_files($css_folder) {
-      
+
       foreach( new DirectoryIterator($this->css_dir) as $stylesheet ) {
         if ( pathinfo($stylesheet->getFilename(), PATHINFO_EXTENSION) == 'css' ) {
           $name = $stylesheet->getBasename('.css') . '-style';
           $uri = get_stylesheet_directory_uri().$css_folder.$stylesheet->getFilename();
           $ver = $stylesheet->getMTime();
 
-          
-          wp_register_style( 
+
+          wp_register_style(
             $name,
             $uri,
             array(),
--- readme.md
+++ readme.md
@@ -1,16 +1,16 @@
 #WP-SCSS
-####A lightweight SCSS compiler for Wordpress. 
+####A lightweight SCSS compiler for Wordpress.
 
 Compiles .scss files on your wordpress install using [lefo's scssphp](https://github.com/leafo/scssphp). Includes settings page for configuring directories, error reporting, compiling options, and auto enqueuing.
 
-The plugin only compiles when changes have been made to the scss files. Compiles are made to the matching css file, so disabling this plugin will not take down your stylesheets. In the instance where a matching css file does not exist yet, the plugin will create the appropriate css file in the css directory. 
+The plugin only compiles when changes have been made to the scss files. Compiles are made to the matching css file, so disabling this plugin will not take down your stylesheets. In the instance where a matching css file does not exist yet, the plugin will create the appropriate css file in the css directory.
 
 ##Settings
 
 ####Directories
-Directories are defined relative to your theme folder. They must be separate from one another, so you cannot define the root folder to compile into itself. 
+Directories are defined relative to your theme folder. They must be separate from one another, so you cannot define the root folder to compile into itself.
 
-Ideally you should setup a scss folder and a css folder within your theme. This will ensure the most accurate compiling. 
+Ideally you should setup a scss folder and a css folder within your theme. This will ensure the most accurate compiling.
 
     library
     |-css
@@ -25,15 +25,15 @@ Compiling comes in three modes:
 
 * Expanded - Full open css. One line per property. Brackets close on their own line.
 * Nested - Lightly compressed css. Brackets close with css block. Indents to match scss nesting.
-* Compressed - Fully compressed css. 
+* Compressed - Fully compressed css.
 
 ####Error Display
-'Show in Header' will post a message on the front end when errors have occured. This helps debug as you write your scss. 
+'Show in Header' will post a message on the front end when errors have occured. This helps debug as you write your scss.
 
 If you're working on a live/production site, you can send errors to a log. This will create a log file in your scss directory and print errors there as they occur. Just keep an eye on it, because the css will not be updated until errors have been resolved.
 
 ####Enqueuing
-The plugin can automatically add your css files to the header for you. This option will [enqueue](http://codex.wordpress.org/Function_Reference/wp_enqueue_style) all files found in the css directory defined in the settings. Keep this in mind if you have other non-compiled css files in this folder. The plugin will add them to the header, just don't reenque them somewhere else. 
+The plugin can automatically add your css files to the header for you. This option will [enqueue](http://codex.wordpress.org/Function_Reference/wp_enqueue_style) all files found in the css directory defined in the settings. Keep this in mind if you have other non-compiled css files in this folder. The plugin will add them to the header, just don't reenque them somewhere else.
 
 Also keep in mind, that if you disable this plugin it can no longer enqueue files for you.
 
@@ -46,27 +46,28 @@ Also keep in mind, that if you disable this plugin it can no longer enqueue file
 You can import other scss files into parent files and compile them into a single css file. To do this, use @import as normal in your scss file. All imported file names *must* start with an underscore. Otherwise they will be compiled into their own css file.
 
 When importing in your scss file, you can leave off the underscore.
-  
+
     @import 'subfile';
 
 ####Compass Support
 Currently there isn't a way to fully support [compass](https://github.com/chriseppstein/compass) with a php compiler. If you want limited support, you can manually import the compass framework. You'll need both the _compass.scss and compass directory.
-    
+
      compass / frameworks / compass / stylesheets /
      @import 'compass';
 
-Alternatively, you can include [Bourbon](https://github.com/thoughtbot/bourbon) in a similar fashion. 
+Alternatively, you can include [Bourbon](https://github.com/thoughtbot/bourbon) in a similar fashion.
 
 ####.sass Support
 This plugin will only work with .scss format.
 
 ##Changelog
+1.1.4 - Add suport for subfolders in scss directory
 1.1.3 - Fix print bug (2) in header
-1.1.2 - Add support for moved wp-content directory  
-1.1.1 - Catch permissions errors   
+1.1.2 - Add support for moved wp-content directory
+1.1.1 - Catch permissions errors
 1.0.0 - Initial Release
 
 ##License
-This plugin is developed and maintained by Connect Think.  
+This plugin is developed and maintained by Connect Think.
 [GPL V3](http://www.gnu.org/copyleft/gpl.html)
 
